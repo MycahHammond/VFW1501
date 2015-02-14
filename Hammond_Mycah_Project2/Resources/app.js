2985d6c78979994ccf682a1fcd19205a37cca858
Mycah Hammond <mlhammond@fullsail.edu>
2015-02-13 20:28:42 -1000

    Friday night

--- Hammond_Mycah_Project2/Resources/app.js
+++ Hammond_Mycah_Project2/Resources/app.js
@@ -1,3 +1,41 @@
+//JSON
+var podcast = {
+	"Title":"Podcasts",	
+	"thinkers":{
+		"title":"Informative",
+		"list":[
+			{
+				"name": "Invisibilia",
+				"description": "Invisibilia is a series about the inivisible forces that touch all of our lives. It explores scientivic research, tying in personal stories to dive deep into the things we can't see that push and pull at us on a daily basis."
+			},
+			{
+				"name": "TED Radio Hour",
+				"description":"Technology, Entertainment, Design. TED pushes the limits on the way we think about everything, offering unique perspectives on a variety of subjects, weekly broadcasts prod at the minds of experts and deep thinkers to offer interesting perspectives."
+			},
+			{
+				"name": "Radio Lab",
+				"description": "Radio Lab explores the world on a different level. Merging science, philosophy, and the human experience to create truly unique and thought provoking content."
+			}
+		]
+		
+	},
+	"Fun":{
+		"title": "Entertainment",
+		"list":[
+			{
+				"name": "StartUp",
+				"description": "StartUp follows Alex Blumberg, formerly of \"This American Life\" and \"Planet Money\", as he podcasts about starting his very own podcasting company. An entertaining and informative look into the side of business we don't often get to see, starting one."
+			},
+			{
+				"name": "All About Android",
+				"description": "Jason Howell, Gina Tapani, and Ron Richards dive into all things Android. With weekly guests, highlights on hardware, news, and apps, they offer an entertaining and comprehensive look into the goings on of the Android world."
+			}
+		]
+		
+	}
+};
+
+
 var mainWindow = Ti.UI.createWindow({
 	backgroundColor: "#f5f5f5"
 	
@@ -5,6 +43,8 @@ var mainWindow = Ti.UI.createWindow({
 
 //Margin var
 var m = "5%";
+var doWork = require("workhorse");
+
 
 var random = [{title: "dude"}, {title:"word"}, {title:"noice!"}, {title:"rad"}];
 
@@ -14,19 +54,24 @@ var titleView = Ti.UI.createView({
 	backgroundColor: "#fff"
 });
 
+var border = Ti.UI.createView({
+	height: 1,
+	backgroundColor: "#333",
+	top: titleView.height + titleView.top	
+});
+
 var titleLable = Ti.UI.createLabel({
 	top: "17%",
-	text: "Random Items",
+	text: podcast.Title,
 	font: {fontSize:35, fontWeight: "bold",}
 });
  
 var newList = Ti.UI.createTableView({
-	data: random,
-	top: titleView.height
+	data: podcast.list,
+	top:  titleView.top + titleView.height + border.height
 });
 
 
-
 titleView.add(titleLable);
-mainWindow.add(titleView, newList);
+mainWindow.add(titleView, border);
 mainWindow.open();
--- Hammond_Mycah_Project2/Resources/workhorse.js
+++ Hammond_Mycah_Project2/Resources/workhorse.js
@@ -1,17 +1,37 @@
-var data = {
-	
-	"slang": [
-		{
-			"1":"Noice",
-			"2":"Right On",
-			"3":"Shoots"
-		},
-		{
-			"1":"Gnarly",
-			"2":"Heavy",
-			"3":"Rictor"	
-		}
-	]
+var podcast = {
+	"thinkers":{
+		"title":"Informative",
+		"list":[
+			{
+				"name": "Invisibilia",
+				"description": "Invisibilia is a series about the inivisible forces that touch all of our lives. It explores scientivic research, tying in personal stories to dive deep into the things we can't see that push and pull at us on a daily basis."
+			},
+			{
+				"name": "TED Radio Hour",
+				"description":"Technology, Entertainment, Design. TED pushes the limits on the way we think about everything, offering unique perspectives on a variety of subjects, weekly broadcasts prod at the minds of experts and deep thinkers to offer interesting perspectives."
+			},
+			{
+				"name": "Radio Lab",
+				"description": "Radio Lab explores the world on a different level. Merging science, philosophy, and the human experience to create truly unique and thought provoking content."
+			}
+		]
+		
+	},
+	"Fun":{
+		"title": "Entertainment",
+		"list":[
+			{
+				"name": "StartUp",
+				"description": "StartUp follows Alex Blumberg, formerly of \"This American Life\" and \"Planet Money\", as he podcasts about starting his very own podcasting company. An entertaining and informative look into the side of business we don't often get to see, starting one."
+			},
+			{
+				"name": "All About Android",
+				"description": "Jason Howell, Gina Tapani, and Ron Richards dive into all things Android. With weekly guests, highlights on hardware, news, and apps, they offer an entertaining and comprehensive look into the goings on of the Android world."
+			}
+		]
+		
+	}
 };
 
-exports.words = data.slang;
+exports.fun = podcast.fun;
+exports.thinkers = podcast.thinkers;

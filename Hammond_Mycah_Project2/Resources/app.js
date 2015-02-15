7953c8f2b0c04bf0f3f58c34d80d2ecd0817ec87
Mycah Hammond <mlhammond@fullsail.edu>
2015-02-14 18:09:07 -1000

    Final App. Project 2

--- Hammond_Mycah_Project2/Resources/app.js
+++ Hammond_Mycah_Project2/Resources/app.js
@@ -1,77 +1,20 @@
-//JSON
-var podcast = {
-	"Title":"Podcasts",	
-	"thinkers":{
-		"title":"Informative",
-		"list":[
-			{
-				"name": "Invisibilia",
-				"description": "Invisibilia is a series about the inivisible forces that touch all of our lives. It explores scientivic research, tying in personal stories to dive deep into the things we can't see that push and pull at us on a daily basis."
-			},
-			{
-				"name": "TED Radio Hour",
-				"description":"Technology, Entertainment, Design. TED pushes the limits on the way we think about everything, offering unique perspectives on a variety of subjects, weekly broadcasts prod at the minds of experts and deep thinkers to offer interesting perspectives."
-			},
-			{
-				"name": "Radio Lab",
-				"description": "Radio Lab explores the world on a different level. Merging science, philosophy, and the human experience to create truly unique and thought provoking content."
-			}
-		]
-		
-	},
-	"Fun":{
-		"title": "Entertainment",
-		"list":[
-			{
-				"name": "StartUp",
-				"description": "StartUp follows Alex Blumberg, formerly of \"This American Life\" and \"Planet Money\", as he podcasts about starting his very own podcasting company. An entertaining and informative look into the side of business we don't often get to see, starting one."
-			},
-			{
-				"name": "All About Android",
-				"description": "Jason Howell, Gina Tapani, and Ron Richards dive into all things Android. With weekly guests, highlights on hardware, news, and apps, they offer an entertaining and comprehensive look into the goings on of the Android world."
-			}
-		]
-		
-	}
-};
-
+//Pull in workhorse
+var doWork = require("workhorse");
 
 var mainWindow = Ti.UI.createWindow({
-	backgroundColor: "#f5f5f5"
+	backgroundColor: "#f5f5f5",
+	title: "Podcasts"
 	
 });
 
-//Margin var
-var m = "5%";
-var doWork = require("workhorse");
-
-
-var random = [{title: "dude"}, {title:"word"}, {title:"noice!"}, {title:"rad"}];
-
-var titleView = Ti.UI.createView({
-	height: "10%",
-	top: "0%",
-	backgroundColor: "#fff"
+//Navigation window (iOS only)
+var navWindow = Ti.UI.iOS.createNavigationWindow({
+	window: mainWindow
 });
 
-var border = Ti.UI.createView({
-	height: 1,
-	backgroundColor: "#333",
-	top: titleView.height + titleView.top	
+var newList = Ti.UI.createTableView({	
 });
 
-var titleLable = Ti.UI.createLabel({
-	top: "17%",
-	text: podcast.Title,
-	font: {fontSize:35, fontWeight: "bold",}
-});
- 
-var newList = Ti.UI.createTableView({
-	data: podcast.list,
-	top:  titleView.top + titleView.height + border.height
-});
-
-
-titleView.add(titleLable);
-mainWindow.add(titleView, border);
-mainWindow.open();
+newList.setData(doWork.sections);
+mainWindow.add(newList);
+navWindow.open();
--- Hammond_Mycah_Project2/Resources/workhorse.js
+++ Hammond_Mycah_Project2/Resources/workhorse.js
@@ -1,37 +1,123 @@
-var podcast = {
-	"thinkers":{
-		"title":"Informative",
-		"list":[
-			{
-				"name": "Invisibilia",
-				"description": "Invisibilia is a series about the inivisible forces that touch all of our lives. It explores scientivic research, tying in personal stories to dive deep into the things we can't see that push and pull at us on a daily basis."
+//JSON
+var casts = {
+		"thinkers":{
+			"name": "INFORMATIVE",
+			"tag": "Deep thinking podcasts.",
+			"list":[
+				{
+					title: "Invisibilia",
+					"description": "Invisibilia is a series about the inivisible forces that touch all of our lives. It explores scientivic research, tying in personal stories to dive deep into the things we can't see that push and pull at us on a daily basis."
+				},
+				{
+					title: "TED Radio Hour",
+					"description":"Technology, Entertainment, Design. TED pushes the limits on the way we think about everything, offering unique perspectives on a variety of subjects, weekly broadcasts prod at the minds of experts and deep thinkers to offer interesting perspectives."
+				},
+				{
+					title: "Radio Lab",
+					"description": "Radio Lab explores the world on a different level. Merging science, philosophy, and the human experience to create truly unique and thought provoking content."
+				}
+			]
+			
 			},
-			{
-				"name": "TED Radio Hour",
-				"description":"Technology, Entertainment, Design. TED pushes the limits on the way we think about everything, offering unique perspectives on a variety of subjects, weekly broadcasts prod at the minds of experts and deep thinkers to offer interesting perspectives."
-			},
-			{
-				"name": "Radio Lab",
-				"description": "Radio Lab explores the world on a different level. Merging science, philosophy, and the human experience to create truly unique and thought provoking content."
-			}
-		]
 		
-	},
-	"Fun":{
-		"title": "Entertainment",
-		"list":[
-			{
-				"name": "StartUp",
-				"description": "StartUp follows Alex Blumberg, formerly of \"This American Life\" and \"Planet Money\", as he podcasts about starting his very own podcasting company. An entertaining and informative look into the side of business we don't often get to see, starting one."
+		"fun":{
+			"name": "ENTERTAINING",
+			"tag": "Easy listening podcasts",
+			"list":[
+				{
+					title: "StartUp",
+					"description": "StartUp follows Alex Blumberg, formerly of \"This American Life\" and \"Planet Money\", as he podcasts about starting his very own podcasting company. An entertaining and informative look into the side of business we don't often get to see, starting one."
+				},
+				{
+					title: "All About Android",
+					"description": "Jason Howell, Gina Tapani, and Ron Richards dive into all things Android. With weekly guests, highlights on hardware, news, and apps, they offer an entertaining and comprehensive look into the goings on of the Android world."
+				}
+			]
+			
 			},
-			{
-				"name": "All About Android",
-				"description": "Jason Howell, Gina Tapani, and Ron Richards dive into all things Android. With weekly guests, highlights on hardware, news, and apps, they offer an entertaining and comprehensive look into the goings on of the Android world."
-			}
-		]
+	};
+	
+//Section Array	
+var sectionInfo = [];
+
+//Description Function
+var babySitter = function(){
+	var descWindow = Ti.UI.createWindow({
+		title: this.title,
+		backgroundColor: "#fff",		
+	});
+	
+	var descView = Ti.UI.createView({
+		backgroundColor: "#545454",
+	
+	});
+	
+	var descLabel = Ti.UI.createLabel({
+		text: this.desc,
+		font: {
+			fontSize: 16,
+			fontStyle: "normal",
+			fontFamily: "Helvetica"
+		},
+		color: "#fff",
+		top: 5, 
+		left:7,
+		right:7,
 		
-	}
+	});
+	
+	descView.add(descLabel);
+	descWindow.add(descView);
+	navWindow.openWindow(descWindow, {animated:true});
 };
 
-exports.fun = podcast.fun;
-exports.thinkers = podcast.thinkers;
+//Creates Sections and Rows. Add Event Listeners.
+for(n in casts){
+	var sectHead = Ti.UI.createView({
+		backgroundColor: "#00ccff"
+	});
+	
+	var headLabel = Ti.UI.createLabel({
+		text: casts[n].name
+	});
+	
+	
+	sectHead.add(headLabel);
+//Footer
+var sectFoot = Ti.UI.createView({
+		backgroundColor: "#545454"
+	});
+	//Footer Label
+	var footLabel = Ti.UI.createLabel({
+		text: casts[n].tag,
+		color: "00ccff",
+	});
+	
+	
+	sectFoot.add(footLabel);
+
+	var section =  Ti.UI.createTableViewSection({
+		headerView: sectHead,
+		footerView: sectFoot
+	});
+	
+	//Build Section Array
+	sectionInfo.push(section);
+	
+	//Rows
+	for(var i=0, j=casts[n].list.length; i<j; i++){
+		var parent = Ti.UI.createTableViewRow({
+			title: casts[n].list[i].title,
+			desc: casts[n].list[i].description,
+			hasChild: true
+		});
+		section.add(parent);
+		parent.addEventListener("click", babySitter);
+	};
+	
+};
+
+exports.sections = sectionInfo;
+
+
+
